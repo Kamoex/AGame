ddc6c5059cda3d254d580e1c69283816424f5769
zhanghe <1120691800@qq.com>
2019-08-04 21:44:21 +0800

    【server-test】 1.添加protocol buffer js 2.添加websocket测试连接

---
+++ Server/test_ptb.ts
@@ -0,0 +1,59 @@
+import { load, Root } from "protobufjs"; // respectively "./node_modules/protobufjs"
+
+export class test_ptb {
+
+    private message: any;
+    private AwesomeMessage: any;
+
+    constructor() {
+
+    }
+
+    public initMsg() {
+        load("./Common/message/test.proto", (err, root: any) => {
+            if (err)
+                throw err;
+            try {
+                this.AwesomeMessage = root.lookupType("TestPackage.TestMessage");
+                this.message = this.AwesomeMessage.create({
+                    sName: "hello",
+                    nId: 1001,
+                    fWeight: 55.7,
+                    bSex: true
+                });
+        
+                console.log(`message = ${JSON.stringify(this.message)}`);
+        
+                let buffer = this.AwesomeMessage.encode(this.message).finish();
+                console.log(`buffer = ${Array.prototype.toString.call(buffer)}`);
+        
+                let decoded = this.AwesomeMessage.decode(buffer);
+                console.log(`decoded = ${JSON.stringify(decoded)}`);
+            } catch (error) {
+                console.log(error);
+                throw error;
+            }
+        });
+    }
+
+    public getMsg(): any {
+        this.message = this.AwesomeMessage.create({
+            sName: "hello",
+            nId: 1001,
+            fWeight: 55.7,
+            bSex: true
+        });
+
+        this.message.sName = "张翮";
+        console.log(`message = ${JSON.stringify(this.message)}`);
+
+        let buffer = this.AwesomeMessage.encode(this.message).finish();
+        console.log(`buffer = ${Array.prototype.toString.call(buffer)}`);
+
+        let decoded = this.AwesomeMessage.decode(buffer);
+        console.log(`decoded = ${JSON.stringify(decoded)}`);
+    }
+}
+
+let ptb: test_ptb = new test_ptb();
+ptb.initMsg();
---
+++ Server/test_websocket.ts
@@ -0,0 +1,46 @@
+import * as http from "http";
+import { server } from "websocket";
+
+const hostname = '127.0.0.1';
+const port = 8001;
+let client: number = 0;
+
+let http_Server: http.Server = http.createServer(function (request, response) {
+    console.log((new Date()) + ' Received request for ' + request.url);
+    response.writeHead(404);
+    response.end();
+});
+http_Server.listen(port, function () {
+    console.log((new Date()) + ' Server is listening on port ' + port);
+});
+let wsServer: server = new server({ httpServer: http_Server, autoAcceptConnections: false });
+
+// 检测客户端连接源
+function originIsAllowed(origin: any) {
+    return true;
+}
+
+wsServer.on('request', function (request) {
+    if (!originIsAllowed(request.origin)) {
+        // Make sure we only accept requests from an allowed origin
+        request.reject();
+        console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
+        return;
+    }
+
+    var connection = request.accept('echo-protocol', request.origin);
+    console.log((new Date()) + ' Connection accepted.');
+    connection.on('message', function (message) {
+        if (message.type === 'utf8') {
+            console.log('Received Message: ' + message.utf8Data);
+            connection.sendUTF(message.utf8Data);
+        }
+        else if (message.type === 'binary') {
+            console.log('Received Binary Message of ' + message.binaryData.length + ' bytes');
+            connection.sendBytes(message.binaryData);
+        }
+    });
+    connection.on('close', function (reasonCode, description) {
+        console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
+    });
+});
