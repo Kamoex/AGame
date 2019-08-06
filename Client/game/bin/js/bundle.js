88b0c0d7a41668fdb37033ba3097bf6d2d5c8c52
zhanghe <1120691800@qq.com>
2019-08-06 23:58:06 +0800

    【client-test】 1.添加客户端websocket测试

---
+++ Client/game/bin/js/bundle.js
@@ -0,0 +1,87 @@
+(function () {
+    'use strict';
+
+    class GameConfig {
+        constructor() { }
+        static init() {
+            var reg = Laya.ClassUtils.regClass;
+        }
+    }
+    GameConfig.width = 640;
+    GameConfig.height = 1136;
+    GameConfig.scaleMode = "fixedwidth";
+    GameConfig.screenMode = "none";
+    GameConfig.alignV = "top";
+    GameConfig.alignH = "left";
+    GameConfig.startScene = "";
+    GameConfig.sceneRoot = "";
+    GameConfig.debug = false;
+    GameConfig.stat = false;
+    GameConfig.physicsDebug = false;
+    GameConfig.exportSceneToJson = true;
+    GameConfig.init();
+
+    class Game {
+        constructor() {
+            Laya.init(600, 400, Laya.WebGL);
+            this.byte = new Laya.Byte();
+            this.byte.endian = Laya.Byte.LITTLE_ENDIAN;
+            this.socket = new Laya.Socket();
+            this.socket.endian = Laya.Byte.LITTLE_ENDIAN;
+            this.socket.connectByUrl("ws://127.0.0.1:8001");
+            this.socket.on(Laya.Event.OPEN, this, this.openHandler);
+            this.socket.on(Laya.Event.MESSAGE, this, this.receiveHandler);
+            this.socket.on(Laya.Event.CLOSE, this, this.closeHandler);
+            this.socket.on(Laya.Event.ERROR, this, this.errorHandler);
+            console.log("Game constructor");
+        }
+        openHandler(event = null) {
+            console.log("connect success");
+        }
+        receiveHandler(msg = null) {
+            console.log("receive " + msg);
+            {
+                this.socket.send("hello world");
+            }
+        }
+        closeHandler(e = null) {
+            console.log("close " + e);
+        }
+        errorHandler(e = null) {
+            console.log("error " + e);
+        }
+    }
+
+    class Main {
+        constructor() {
+            if (window["Laya3D"])
+                Laya3D.init(GameConfig.width, GameConfig.height);
+            else
+                Laya.init(GameConfig.width, GameConfig.height, Laya["WebGL"]);
+            Laya["Physics"] && Laya["Physics"].enable();
+            Laya["DebugPanel"] && Laya["DebugPanel"].enable();
+            Laya.stage.scaleMode = GameConfig.scaleMode;
+            Laya.stage.screenMode = GameConfig.screenMode;
+            Laya.stage.alignV = GameConfig.alignV;
+            Laya.stage.alignH = GameConfig.alignH;
+            Laya.URL.exportSceneToJson = GameConfig.exportSceneToJson;
+            if (GameConfig.debug || Laya.Utils.getQueryString("debug") == "true")
+                Laya.enableDebugPanel();
+            if (GameConfig.physicsDebug && Laya["PhysicsDebugDraw"])
+                Laya["PhysicsDebugDraw"].enable();
+            if (GameConfig.stat)
+                Laya.Stat.show();
+            Laya.alertGlobalError = true;
+            Laya.ResourceVersion.enable("version.json", Laya.Handler.create(this, this.onVersionLoaded), Laya.ResourceVersion.FILENAME_VERSION);
+        }
+        onVersionLoaded() {
+            Laya.AtlasInfoManager.enable("fileconfig.json", Laya.Handler.create(this, this.onConfigLoaded));
+        }
+        onConfigLoaded() {
+            GameConfig.startScene && Laya.Scene.open(GameConfig.startScene);
+        }
+    }
+    new Main();
+    new Game();
+
+}());
---
+++ Client/game/src/Game.ts
@@ -0,0 +1,54 @@
+export class Game {
+    private socket: Laya.Socket;
+    private byte: Laya.Byte;
+    constructor() {
+        //初始化引擎
+        Laya.init(600, 400, Laya.WebGL);
+        this.byte = new Laya.Byte();
+        //这里我们采用小端
+        this.byte.endian = Laya.Byte.LITTLE_ENDIAN;
+        this.socket = new Laya.Socket();
+        //这里我们采用小端
+        this.socket.endian = Laya.Byte.LITTLE_ENDIAN;
+        //建立连接
+        this.socket.connectByUrl("ws://127.0.0.1:8001");
+        this.socket.on(Laya.Event.OPEN, this, this.openHandler);
+        this.socket.on(Laya.Event.MESSAGE, this, this.receiveHandler);
+        this.socket.on(Laya.Event.CLOSE, this, this.closeHandler);
+        this.socket.on(Laya.Event.ERROR, this, this.errorHandler);
+console.log("Game constructor");
+    }
+    private openHandler(event: any = null): void {
+        //正确建立连接；
+        console.log("connect success");
+    }
+    private receiveHandler(msg: any = null): void {
+        ///接收到数据触发函数
+        console.log("receive " + msg);
+        let a = true;
+        if(a) {
+            this.socket.send("hello world");//这是发送字符串的形式。
+        }
+        else {
+            this.byte.writeByte(1);//写入一个字节
+            this.byte.writeInt16(20);//写入一个int16的数据
+            this.byte.writeFloat32(20.5);//写入一个32位的浮点数据
+            this.byte.writeUTFString("hello");// 写入一个字符串；
+            var by:Laya.Byte = new Laya.Byte();//这里声明一个临时Byte类型
+            by.endian = Laya.Byte.LITTLE_ENDIAN;//设置endian；
+            by.writeInt32(5000);//写入一个int32数据
+            by.writeUint16(16);//写入一个uint16 数据
+            this.byte.writeArrayBuffer(by.buffer);//把临时字节数据的数据写入byte中，这里注意写入的是by.buffer;
+            this.socket.send(this.byte.buffer);//这里是把字节数组的数据通过socket发送给服务器。
+            this.byte.clear();//清除掉数据;方便下次读写；
+        }
+    }
+    private closeHandler(e: any = null): void {
+        //关闭事件
+        console.log("close " + e);
+    }
+    private errorHandler(e: any = null): void {
+        //连接出错
+        console.log("error " + e);
+    }
+}
